30030e2011c9ffbf62facbee86bb186ef28604d1
karboom <mail@karboom.me>
2016-01-14 11:28:31 +0800

    feat: Add judgement for `package.json`

--- generators/app/index.js
+++ generators/app/index.js
@@ -6,55 +6,64 @@ var glob = require('glob');
 var mkdirp = require('mkdirp');
 
 module.exports = yeoman.generators.Base.extend({
-  prompting: function () {
-    var self = this;
-    var done = self.async();
+	prompting: function () {
+		var self = this;
+		var done = self.async();
 
-    this.log(chalk.blue("Please init npm package first!"));
+		var choose = function () {
+			self.log(yosay(
+				'Welcome to the best ' + chalk.red('generator-restful-api') + ' generator!'
+			));
 
-    this.spawnCommand("npm", ["init"]).on('close', function () {
-      self.log(yosay(
-        'Welcome to the best ' + chalk.red('generator-restful-api') + ' generator!'
-      ));
+			var prompts = [{
+				type: 'rawlist',
+				choices: ['express', 'restify', 'koa'],
+				name: 'framework',
+				message: 'Which framework do you want to use?',
+				default: 'koa'
+			}];
 
-      var prompts = [{
-        type: 'rawlist',
-        choices: ['express', 'restify', 'koa'],
-        name: 'framework',
-        message: 'Which framework do you want to use?',
-        default: 'koa'
-      }];
+			self.prompt(prompts, function (props) {
+				self.props = props;
 
-      self.prompt(prompts, function (props) {
-        self.props = props;
+				done();
+			}.bind(self));
+		};
 
-        done();
-      }.bind(this));
-    });
-  },
+		if (!this.fs.exists(this.destinationPath('package.json'))) {
+			this.log(chalk.blue("Please init npm package first!"));
 
-  writing: function () {
-    this.fs.copy(
-      this.templatePath(),
-      this.destinationPath()
-    );
-    mkdirp('routers');
-    this.log(chalk.yellow("create" ) + " routers");
-    mkdirp('test/routers');
-    this.log(chalk.yellow("create" ) + " test/routers");
+			this.spawnCommand("npm", ["init"]).on('close', choose);
+		} else {
+			choose();
+		}
 
-    this.fs.copyTpl(
-      this.templatePath('app.js'),
-      this.destinationPath('app.js'),
-      {
-        framework: this.props.framework,
-        creation: this.props.framework == 'restify' ? '.createServer()' : '()'
-      }
-    );
-  },
+	},
 
-  install: function () {
-    this.log(chalk.gray("Installing "+ this.props.framework));
-    this.npmInstall(this.props.framework, {save: true});
-  }
+	writing: function () {
+		var self = this;
+		this.fs.copy(
+			this.templatePath(),
+			this.destinationPath()
+		);
+
+		['routers', 'test/routers', 'test/utils'].forEach(function (dir) {
+			mkdirp(dir);
+			self.log(chalk.yellow("create") + " " + dir);
+		});
+
+		this.fs.copyTpl(
+			this.templatePath('app.js'),
+			this.destinationPath('app.js'),
+			{
+				framework: this.props.framework,
+				creation: this.props.framework == 'restify' ? '.createServer()' : '()'
+			}
+		);
+	},
+
+	install: function () {
+		this.log(chalk.blue("Installing " + this.props.framework));
+		this.npmInstall(this.props.framework, {save: true});
+	}
 });
--- generators/app/templates/app.js
+++ generators/app/templates/app.js
@@ -1,5 +1,6 @@
 var <%= framework %> = require('<%= framework %>');
 var app = <%= framework %><%= creation %>;
+var mid = require('utils/middleware');
 
 
 // Write your middleware and routers register here
